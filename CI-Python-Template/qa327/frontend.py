be63fd4a5099f803157600e364c7e06dfde7951e
alina padoun <alinapadoun2@gmail.com>
2020-11-07 10:23:42 -0500

    Added functionality to frontend.py to meet R8 and added 404.html

--- CI-Python-Template/qa327/frontend.py
+++ CI-Python-Template/qa327/frontend.py
@@ -156,3 +156,9 @@ def profile(user):
     # front-end portals
     tickets = bn.get_all_tickets()
     return render_template('index.html', user=user, tickets=tickets)
+
+
+@app.errorhandler(404)
+def other_requests(error):
+    # returns a 404 error for any other requests
+    return render_template('404.html', message='404 ERROR: The requested URL was not found on the server.'), 404
