4f44649fdc0fbb0eb7ea9bbd449f051613447783
ETSnider <71848266+ETSnider@users.noreply.github.com>
2020-11-15 01:04:55 -0500

    Update test_index.py

--- CI-Python-Template/qa327_test/frontend/test_index.py
+++ CI-Python-Template/qa327_test/frontend/test_index.py
@@ -4,6 +4,7 @@ from seleniumbase import BaseCase
 from qa327_test.conftest import base_url
 from unittest.mock import patch
 from qa327.models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 """
 This file defines all unit tests for the frontend index.
