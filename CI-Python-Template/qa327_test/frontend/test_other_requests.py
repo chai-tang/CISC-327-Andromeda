5e8159fd6bef3b091bfcdb12a3eba7daf033c200
alina padoun <alinapadoun2@gmail.com>
2020-11-16 21:26:04 -0500

    Small update to test_other_requests.py

--- CI-Python-Template/qa327_test/frontend/test_other_requests.py
+++ CI-Python-Template/qa327_test/frontend/test_other_requests.py
@@ -1,14 +1,15 @@
 import pytest
 import requests
 from seleniumbase import BaseCase
+#cd onedrive/desktop/cisc327/CISC* 
 
 from qa327_test.conftest import base_url
 
 invalid_paths = [
     '/asdfasdfasdfdasdf',
     '/%20',
-    '/-------'
-    '/loginx'
+    '/-------',
+    '/loginx',
     '/lohout'
 ]
 
@@ -30,17 +31,21 @@ class FrontEndIndexTest(BaseCase):
         - The page should show an error message and the browser should receive an 'HTTP 404' response
         
         """
+        
         # visit ever invalid path in 'invalid_paths'
         for path in invalid_paths:
-            response = requests.get(base_url + path)
-            self.assert_equal(response.status_code, 404, response.status_code)
              # open invalid path
             self.open(base_url + path)
             # validate that error message is shown
             self.assert_element("#message", timeout=default_timeout)
             self.assert_text("404 ERROR: The requested URL was not found on the server.", "#message", timeout=default_timeout)
+
+        # visit ever invalid path in 'invalid_paths'
+        for path in invalid_paths:
             # validate that browser received a HTTP 404 response
-            #response = requests.get(base_url + path)
-            #self.assert_equal(response.status_code, 404, response.status_code)
+            r = requests.get(base_url + path)
+            #self.assert_equal(r.status_code, 404, response.status_code)
             # or
-            #assert response.status_code == 404
+            assert r.status_code == 404
+        
+        # it only works when they are in seperate for loops, not sure why
