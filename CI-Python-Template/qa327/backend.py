5d02e3133b75f9f771de78bf7c78a5874f9c8ecd
Sam Zhang <zzq1015@users.noreply.github.com>
2020-12-12 22:29:27 -0500

    minor change

--- CI-Python-Template/qa327/backend.py
+++ CI-Python-Template/qa327/backend.py
@@ -40,8 +40,6 @@ def register_user(email, name, password, password2):
     :param password2: another password input to make sure the input is correct
     :return: an error message if there is any, or None if register succeeds
     """
-    if (password != password2):
-        return "Passwords does not match"
     hashed_pw = generate_password_hash(password, method='sha256')
     # Store the encrypted password rather than the plain password
     new_user = User(email=email, name=name, password=hashed_pw, balance=0)
